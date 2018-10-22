2ef81d21ec04211b0d2bc154b9df95da5f56adf6
RamyaGonchigar <ramyagonchi@gmail.com>
2018-10-22 10:27:10 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    EventsComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
